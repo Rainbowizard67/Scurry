1b4446d88163dc27624f263bd12d594252ad2de1
Rainbowizard67 <goboysgo42@gmail.com>
2026-01-18 14:03:14 -0500

    Updated the env file and bash script dependency file

--- config/setup_script.sh
+++ config/setup_script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-###Start Function declaration###
+#============================#
+# Start Function declaration #
+#============================#
 
 find_config() {
     local env_file="$1"
@@ -8,25 +10,80 @@ find_config() {
     echo
 }
 
-###End Function Declaration###
+get_distro() {
+    distro=$(lsb_release -i | sed 's/^Distributor ID:[[:space:]]*//')
+    echo "$distro"
+}
+
+debian_depend_install() {
+    sudo apt update
+    sudo apt install valgrind gdb build-essential libyaml-dev libcyaml-dev libssl-dev cmake -y
+}
+
+arch_depend_install() {
+    sudo pacman -Syu --noconfirm
+    sudo pacman -Sy valgrind gdb base-devel libcyaml openssl cmake --noconfirm
+}
+
+depend_switch() {
+    local distro="$1"
+    case "$distro" in
+	Ubuntu|Debian)
+	    debian_depend_install
+	    ;;
+	Fedora|Redhat)
+	    ;;
+	Arch)
+	    arch_depend_install
+	    ;;
+	*)
+	    printf "\e[31mUnsupported distro: $distro"
+	    return 1
+	    ;;
+    esac
+}
+
+create_program_env() {
+    local ENV_FILE="scurry.env"
+
+    cat > "$ENV_FILE" <<EOL
+###Scurry Env###
+DISTRO_TYPE="$1"
+EOL
+
+    find_config "$ENV_FILE"
+}
+
+#==========================#
+# End Function Declaration #
+#==========================#
+
+#=========================#
+# Starting Main of Script #
+#=========================#
+
+distro=$(get_distro)
 
 sudo printf "%b\n" "\e[0;34mDownloading and Installing dependencies...\e[0m"
 
-sudo apt update
-sudo apt install valgrind gdb build-essential libyaml-dev libcyaml-dev -y
+depend_switch "$distro"
 
 sudo printf "%b\n" "\e[0;34mFinished dependency install\e[0m"
 
+echo
+
 sudo printf "%b\n" "\e[0;34mStarting env file initialization...\e[0m"
 
-ENV_FILE="scurry.env"
+echo "."
+sleep 1
+echo "."
+sleep 1
+echo "."
 
-cat > "$ENV_FILE" <<EOL
-###Scurry Env###
-
-EOL
-
-find_config "$ENV_FILE"
+create_program_env "$distro"
 
 sudo printf "%b\n" "\e[0;34mFinished env file initialization\e[0m"
 
+#=======================#
+# Ending Main of Script #
+#=======================#
