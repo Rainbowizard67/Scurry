308e00db9b8335f9890503d0437de70a098a2a2c
Rainbowizard67 <goboysgo42@gmail.com>
2026-06-04 19:31:24 -0400

    finished portion for now
    Signed-off-by: Rainbowizard67 <goboysgo42@gmail.com>

--- src/proxy/proxy.c
+++ src/proxy/proxy.c
@@ -1,5 +1,9 @@
 #include "../include/proxy.h"
 
+static void set_nonblocking(int sock) {
+	int flags = fcntl(sock, F_GETFL, 0);
+	fcntl(sock, F_SETFL, flags | O_NONBLOCK);
+}
 
 void create_proxy_event() {
 
