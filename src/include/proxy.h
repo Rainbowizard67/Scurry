303f95cdc194812cf7a59a7037fa6e8848dd49bc
Rainbowizard67 <goboysgo42@gmail.com>
2026-06-04 19:20:30 -0400

    Added the files

---
+++ src/include/proxy.h
@@ -0,0 +1,11 @@
+#ifndef PROXY_H
+#define PROXY_H
+
+#include "project_headers.h"
+#include "scurry_utils.h"
+#include <stdio.h>>
+#include <string.h>
+
+#define BACKLOG 30
+
+#endif
---
+++ src/proxy/proxy.c
@@ -0,0 +1,11 @@
+#include "../include/proxy.h"
+
+
+void create_proxy_event() {
+
+
+}
+
+void main_event_loop(int epoll_fd, int server_soc) {
+
+}
